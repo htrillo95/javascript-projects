67b7a9e61c184d7f3d2adb136dfc68207a855e0b
Hector Trillo <hectortrillo@Hectors-MacBook-Air.local>
2024-01-31 21:17:16 -0500

    CHP.12 EX

--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -7,11 +7,21 @@ function randomSelection(arr){
  
  let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
  
- for (i=0; i < 8; i++){
+ for (i=0; i < 3; i++){
    console.log(randomSelection(happiness));
+   console.log(randomSelection(words));
  }
- 
+ console.log("-----------------")
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
  //b) Have the code randomly pick one array, and then print 2 random items from it.
+ let chosenArray = randomSelection([happiness, words]);
+
+for (let i = 0; i < 2; i++) {
+  console.log(randomSelection(chosenArray));
+}
+console.log("-----------------")
  //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+ let newArray = [randomSelection(happiness), randomSelection(words)];
+
+console.log(newArray);
--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,21 +2,80 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)}
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.floor(Math.random()*11)}
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+ 
+ let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+ 
+ let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.floor(Math.random()*11)}
+};
 
-// After you have created the other object literals, add the astronautID property to each one.
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
+
+function crewReports(animal) {
+   return `${animal.name} is a ${animal.species}. 
+   They are ${animal.age} years old and ${animal.mass} kilograms. 
+   Their ID is ${animal.astronautID}.`;}
+
+   for (let member of crew) {
+      console.log(crewReports(member));
+  } 
+  // After you have created the other object literals, add the astronautID property to each one.
 
 // Create an array to hold the animal objects.
 
 // Print out the relevant information about each animal.
 
-// Start an animal race!
+// Start an animal race
+
+function fitnessTest(crew){
+   let results = [], numSteps, turns;
+   for (let i = 0; i < crew.length; i++){
+       numSteps = 0;
+       turns = 0;
+       while(numSteps < 20){
+       numSteps += crew[i].move();
+       turns++;
+       }
+       results.push(`${crew[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+  }
+
+  let resultsArray = fitnessTest(crew);
+for (let result of resultsArray) {
+    console.log(result);
+}
