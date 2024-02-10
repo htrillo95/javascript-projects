cad63fd6e259b8c13471272de5d02d0624b1a9a2
Hector Trillo <hectortrillo@Hectors-MacBook-Air.local>
2024-02-10 12:43:14 -0500

    chp 17. Exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,7 +1,14 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
+    //my added code
+    let result;
+    try {
     let result = lab.runLab(3);
+    } catch (error) {
+      result = "Error thrown";
+    }
+    //added code ends
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
@@ -22,3 +29,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
