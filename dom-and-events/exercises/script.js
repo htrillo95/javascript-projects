92bc2a117e7d36955801f8d87a73b664f0b3a8b2
Hector Trillo <hectortrillo@Hectors-MacBook-Air.local>
2024-02-24 18:04:53 -0500

    dom update

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+     
+    missionAbort.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "red";
+    });
+
+    // Event listener for mouseout
+    missionAbort.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function(event) {
+        const confirmAbort = window.confirm("Are you sure you want to abort the mission?");
+            if (confirmAbort) {
+        paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.';
+        }
+    });
 }
 
 window.addEventListener("load", init);
